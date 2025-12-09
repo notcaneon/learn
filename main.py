03ef4592142a65ebbdafcc3ed1095a8c3cd4fe2e
notcaneon <caneonlol@gmail.com>
2025-12-09 21:55:39 +0545

    Snake Movement

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 from turtle import Turtle, Screen
+import time
 screen = Screen()
-x_cord = -40
+
 turtles = []
 screen.bgcolor("black")
-screen.setup(width=1000,height=800)
-for turtle in range(1,4):
-    turtle = Turtle()
-    turtle.shape("square")
-    turtle.goto(x=x_cord,y=0)
-    x_cord += 20
-    turtle.color("white")
-    turtles.append(turtle)
-    print(x_cord)
+screen.setup(width=1000,height=600)
+starting_positions = [-20,0,20]
+screen.tracer(0)
+for position in range(0,3):
+        segment = Turtle()
+        segment.shape("square")
+        segment.goto(x=starting_positions[position],y=0)
+        segment.penup()
+        turtles.append(segment)
+        segment.color("white")
+
+def turn_forwards():
+        turtles[0].setheading(90)
+
+def turn_backwards():
+        turtles[0].setheading(270)
+
+def turn_left():
+        turtles[0].setheading(180)
+
+def turn_right():
+        turtles[0].setheading(0)
+
+screen.listen()
+screen.onkey(turn_forwards, "w")
+screen.onkey(turn_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+
+
+
+game_running = True
+while game_running:
+        screen.update()
+        for turtle in range(len(turtles)-1,0,-1): 
+                turtles[turtle].goto(turtles[turtle-1].pos())
+                time.sleep(0.03) 
+        turtles[0].forward(20)
+
+screen.title("Snake Game")
+
 screen.exitonclick()
